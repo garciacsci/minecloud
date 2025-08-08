01ce404d29489bc8eb3757295d3e0bf28239e024
Chris Garcia <garciacsci@users.noreply.github.com>
2025-08-07 22:53:48 -0700

    feat: update minecraft server download url to 1.18.2 jar

--- minecloud_configs/advanced_configs/minecraft-server-download-url.ts
+++ minecloud_configs/advanced_configs/minecraft-server-download-url.ts
@@ -1,3 +1,4 @@
-// Minecraft 1.21.4 Server
 export const MINECRAFT_SERVER_DOWNLOAD_URL =
-  'https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d548d8c001/server.jar';
+  // 'https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d548d8c001/server.jar'; // Minecraft 1.21.4 Server
+
+  'https://piston-data.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar'; // Minecraft 1.18.2 Server
